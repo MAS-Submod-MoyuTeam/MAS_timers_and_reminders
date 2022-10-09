9602a60c797ab994af2588b44094ac2658d89b2e
dreamscached <dreamscache.d@gmail.com>
2022-10-09 23:34:01 +0200

    Build scripts

---
+++ scripts/build.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+dir="$(dirname "$(CDPATH="" cd -- "$(dirname -- "$0")" && pwd)")"
+temp="$(mktemp -d)"
+
+build="$dir/build"
+mkdir -p "$build"
+
+name="$(perl -ne 'if (/^.*name="([^"]*)"/) { print $1; exit }' "$dir/mod/reminder_header.rpy")"
+version="$(perl -ne 'if (/^.*version="([^"]*)"/) { print $1; exit }' "$dir/mod/reminder_header.rpy")"
+package="$(echo "$name" | tr "[:upper:]" "[:lower:]" | tr "[:blank:]" "-")"
+
+mod="$temp/game/Submods/$name"
+mkdir -p "$mod"
+
+cp -r "$dir/mod"/* "$mod"
+
+(cd "$temp" || exit 1; find game | zip -9@q "$build/$package-$version.zip" && rm -rf "$temp")
